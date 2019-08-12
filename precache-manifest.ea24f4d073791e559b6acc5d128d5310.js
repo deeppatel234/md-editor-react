7494504eea0c8905c7c6a09be602cb7fda93949c
Deep Patel <deeppatel234@gmail.com>
2019-08-12 12:56:31 +0530

    Updates

---
+++ precache-manifest.ea24f4d073791e559b6acc5d128d5310.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2a48368b81c2636b7194b3f17c84a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cfe42e94fc3629f6329",
+    "url": "/static/css/2.efbe1382.chunk.css"
+  },
+  {
+    "revision": "e90529763d9dbe734d97",
+    "url": "/static/css/main.097b39cc.chunk.css"
+  },
+  {
+    "revision": "7cfe42e94fc3629f6329",
+    "url": "/static/js/2.aaf1e422.chunk.js"
+  },
+  {
+    "revision": "e90529763d9dbe734d97",
+    "url": "/static/js/main.9f97120d.chunk.js"
+  },
+  {
+    "revision": "6db26e6357ebef89ec53",
+    "url": "/static/js/runtime~main.9a890e17.js"
+  }
+]);
