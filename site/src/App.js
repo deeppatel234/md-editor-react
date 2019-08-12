f79b922d9009d6fe55e79b0ea4ee6c0f8a800f55
Deep Patel <deeppatel234@gmail.com>
2019-08-12 12:52:49 +0530

    imporve style and add site

--- site/src/App.js
+++ site/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { MDEditor } from 'md-editor-react';
+import hljs from 'highlight.js';
+
+import 'highlight.js/styles/github-gist.css';
+import 'codemirror/mode/gfm/gfm';
+import 'codemirror/lib/codemirror.css';
+import 'md-editor-react/dist/style.css';
+
+import './style.css';
+
+import { HeartIcon, GithubIcon } from './icons';
 
 function App() {
   return (
-    <div className="App">
-      Hello Site
+    <div className="site-wrapper">
+      <div class="header">
+        <div className="left">
+          <img src="/logo.png" /> <span className="title">Markdown Editor React</span>
+        </div>
+        <div className="right">
+          <a href="https://github.com/deeppatel234/md-editor-react" target="_blank" className="links"><GithubIcon /> Github</a>
+        </div>
+      </div>
+      <div class="body">
+        <MDEditor
+          defaultValue="Hello World"
+          parserOptions={{
+            highlight: code => hljs.highlightAuto(code).value,
+          }}
+        />
+      </div>
+      <div className="footer">
+        Made with <HeartIcon /> in India
+      </div>
     </div>
   );
 }
